b76f583f1317cf964a59c33924bbdaf542895e42
Manuela Mercado <mmercado@intellisys.com.do>
2020-03-24 08:19:01 -0400

    Adding filters (#4)

--- app/src/main.js
+++ app/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.filter('placeTH', function (value) {
+  if(value === 1){ return `${value}st`;}
+  else if(value === 2){ return `${value}nd`;}
+  else if(value === 3){ return `${value}rd`;}
+  else { return `${value}th`;}
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
